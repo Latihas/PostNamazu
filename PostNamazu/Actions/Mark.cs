60608de4b1c3031fa836bbe05d78c2fc721d865c
Latihas <lyjp2002@163.com>
2026-05-27 15:55:58 +0800

    7.50.1.12

--- PostNamazu/Actions/Mark.cs
+++ PostNamazu/Actions/Mark.cs
@@ -62,7 +62,7 @@ internal class Mark : NamazuModule {
 			};
 			return actor;
 		}
-		var combatants = FFXIV_ACT_Plugin.DataRepository.GetCombatantList().Where(i => !string.IsNullOrEmpty(i.Name) && i.ID != 0xE0000000);
+		var combatants = FFXIV_ACT_Plugin.DataRepository.GetCombatantList().Where(i => !string.IsNullOrEmpty(i.Name) && i.ID != 0xE0000000).ToList();
 		return combatants.FirstOrDefault(i => i.ID == id)
 		       ?? combatants.FirstOrDefault(i => i.Name == name)
 		       ?? throw new Exception(L.Get("Mark/ActorNotFound", id?.ToString("X8") ?? name ?? "(null)"));
